503f735cf37390f178b9de9f2dd6c4463b52fa4d
Mar'i Fauzan <marifauzan20@gmail.com>
2021-11-16 15:50:36 +0700

    add router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React from "react";
 
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home";
+import About from "./pages/About";
+import Location from "./pages/Location";
 
 function App() {
   return (
     <BrowserRouter>
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="location" element={<Location />} />
+        <Route path="about" element={<About />} />
+      </Routes>
     </BrowserRouter>
   );
 }
