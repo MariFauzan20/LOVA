0231c81d93b79c8d287f18620f8ad7f9efaec375
Deni Alpiana <Denzalv01@gmail.com>
2021-11-16 18:56:04 +0700

    update pencarian

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 // import Home from "./pages/Home";
-import Location from "./pages/Location";
+import Location from "./pages/LocationPage";
 // import List from "./components/List";
 // import About from "./pages/About";
 
--- src/components/Button.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function Button() {
-  return <h1>Ini button</h1>;
-}
